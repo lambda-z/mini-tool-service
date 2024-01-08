5d363b282011389afee5ea11977b237b293777c7
zhangchuan <chuan@aspecta.id>
2024-01-08 23:41:14 +0800

    :sparkle: Added table.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  Home Page
-  {{model}}
 
-  <el-card>
-    <el-button>按钮</el-button>
-  </el-card>
+  <el-table :data="tableData" style="width: 100%">
+
+      <el-table-column v-for="item in columns" :label="item.label" width="180">
+          <template #default="scope">
+              <div style="display: flex; align-items: center">
+                  <span style="margin-left: 10px">{{ scope.row.date }}</span>
+              </div>
+          </template>
+      </el-table-column>
+
+  </el-table>
 
 </template>
 
 <script setup lang="ts">
-
 import {ref} from "vue";
 
-const model = ref("111")
+const columns = ref([
+    {
+        label: "ID",
+    },
+    {
+        label: "name",
+    },
+    {
+        label: "height",
+    },
+])
 
 </script>
 
